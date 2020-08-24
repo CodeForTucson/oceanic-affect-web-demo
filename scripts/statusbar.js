c856d5de1ece20cf54582fd6f39045b1ecaaba2a
Daniel Stormont <daniel.stormont@gmail.com>
2020-08-23 23:45:10 -0700

    Updated status bar.

--- scripts/statusbar.js
+++ scripts/statusbar.js
@@ -2,12 +2,21 @@ var data = [
     {
       type: "indicator",
       mode: "gauge",
-      gauge: { shape: "bullet" },
-      delta: { reference: 100 },
-      value: 65,
-      domain: { x: [0, 1], y: [0, 1] }
+      value: 160,
+      domain: { x: [0, 1], y: [0, 1] },
+      gauge: {
+        shape: "bullet",
+        axis: { range: [150, 1150], visible: false },
+        bgcolor: "cyan",
+        steps: [
+          { range: [350, 750], color: "yellow" }, { range: [750, 1150], color: "red"}],
+        bar: { color: "darkblue" }
+      }
     }
   ];
   
-  var layout = { width: 1076, height: 200 };
-  Plotly.newPlot('statusbarid', data, layout);
+  var layout = { width: 1070, height: 200 };
+  var config = { displayModeBar: false, responsive: false };
+  
+  Plotly.newPlot('statusbarid', data, layout, config);
+  
